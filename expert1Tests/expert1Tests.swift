7da2cc8d80910396299e15d68c89136dad8b0712
Hafid Ali Mustaqim <hafidalimustaqim13@gmail.com>
2025-04-02 21:33:31 +0700

    add test

---
+++ expert1Tests/expert1Tests.swift
@@ -0,0 +1,16 @@
+//
+//  expert1Tests.swift
+//  expert1Tests
+//
+//  Created by Hafid Ali Mustaqim on 02/04/25.
+//
+
+import Testing
+
+struct expert1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
